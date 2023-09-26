8ffac89a1fb8714251835959a7bd3b87227d1e2f
shivam ratnam <shivamratnam08@gmail.com>
2023-09-26 13:22:29 +0530

    InitialCommit

---
+++ demo_inApp/DestinationView.swift
@@ -0,0 +1,8 @@
+//
+//  DestinationView.swift
+//  demo_inApp
+//
+//  Created by Shivam Ratnam on 19/09/23.
+//
+
+import Foundation
---
+++ demo_inApp/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  demo_inApp
+//
+//  Created by Shivam Ratnam on 19/09/23.
+//
+
+import SwiftUI
+
+struct DestinationView: View {
+    var body: some View {
+        Text("This is the destination view")
+    }
+}
+
+struct DestinationView_Previews: PreviewProvider {
+    static var previews: some View {
+        DestinationView()
+    }
+}
